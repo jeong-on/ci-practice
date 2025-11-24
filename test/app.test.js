794c13254e0fb6abbb9a4af98b1f76f5d52ae7ab
Ubuntu <ubuntu@ip-172-31-35-45.ap-northeast-2.compute.internal>
2025-11-24 06:27:02 +0000

    app.test.js 수정

--- test/app.test.js
+++ test/app.test.js
@@ -9,8 +9,8 @@ app.get('/', (req, res) => {
 describe('GET /', () => {
     it('should return Hello, World!', async () => {
         const res = await request(app).get('/');
-        expect(res.statusCode).toEquals(200);
-        expect(res.text).toEquals('Hello, World!');
+        expect(res.statusCode).toBe(200);
+        expect(res.text).toBe('Hello, World!');
     });
 });
 
